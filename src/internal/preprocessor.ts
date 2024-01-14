a00f5f39537fd2c0cfa940f3a2f320dbe2db4cb0
Wlad Gumenyuk <wlad@gumenyuk.de>
2024-01-14 21:29:14 +0100

    feat: add `plugins` to `PaperfeelOptions` type

--- src/internal/preprocessor.ts
+++ src/internal/preprocessor.ts
@@ -1,10 +1,16 @@
 import type { PreprocessorGroup } from "svelte/compiler";
 import type { VitePreprocessOptions } from "@sveltejs/vite-plugin-svelte";
+import type { Plugin, Settings as PluginSettings } from "unified";
 
 /**
     Paperfeel preprocessor options.
 */
 export type PaperfeelOptions = {
+    /**
+        Plugins passed to `unified.js`.
+    */
+    plugins?: (Plugin | [ Plugin, PluginSettings ])[];
+
     /**
         Options passed to `@sveltejs/vite-plugin-svelte`.
     */
