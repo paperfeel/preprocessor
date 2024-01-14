314921b087edf473efede8dde77e4fd90904c52f
Wlad Gumenyuk <wlad@gumenyuk.de>
2024-01-14 22:09:07 +0100

    feat: add markdown processor

---
+++ src/internal/markdown.ts
@@ -0,0 +1,43 @@
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
+
+// Types
+import type { PaperfeelOptions } from "$internal/preprocessor";
+
+/**
+    Filters user plugins.
+*/
+const filterPlugins = (
+    plugins: PaperfeelOptions["plugins"],
+    filter: string
+) => {
+    if(!plugins) {
+        return [];
+    }
+
+    return plugins.filter((plugin) => {
+        const name = Array.isArray(plugin) ? plugin[0].name : plugin.name;
+        return name.startsWith(filter);
+    });
+};
+
+/**
+    Creates an instance of a Markdown processor.
+*/
+export const createMarkdownProcessor = (
+    plugins?: PaperfeelOptions["plugins"]
+) => {
+    const remarkUserPlugins = filterPlugins(plugins, "remark");
+    const rehypeUserPlugins = filterPlugins(plugins, "rehype");
+
+    const processor = unified()
+        .use(remarkParse)
+        .use(remarkUserPlugins)
+        .use(remarkRehype)
+        .use(rehypeUserPlugins)
+        .use(rehypeStringify);
+
+    return processor;
+};
